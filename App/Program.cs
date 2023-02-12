e265e7b26cfee980601a754552f5597805de7afd
Mariia <tishchenko2014@mail.ru>
2023-02-12 13:48:25 +0300

    init my project

---
+++ App/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using Geometry;
+
+
+Circle circle = new Circle(5);
+Triangle triangle = new Triangle(3,4,5);
+
+Console.WriteLine(circle.GetSquare());
+Console.WriteLine(triangle.GetSquare());
+Console.ReadLine();
+
